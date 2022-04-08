3ba7a51b968c68a72b68c8fcf196d246b38b79e7
gitenxx <empty>
2022-04-08 17:50:11 +0300

    first commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-page class="flex flex-center">
+    <div>
+          <InputPage />
+        </div>
+  </q-page>
+</template>
+
+<script>
+// import { defineComponent } from 'vue'
+
+// export default defineComponent({
+//   name: 'IndexPage'
+// })
+
+import InputPage from "../components/InputPage";
+export default {
+  name: 'IndexPage',
+  components: {InputPage}
+}
+</script>
+
